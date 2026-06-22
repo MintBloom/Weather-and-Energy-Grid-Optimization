ed26ffab4063a8141f05a87e7bee92470e00daff
MintBloom <a.stegmann09@gmail.com>
2026-06-22 16:07:16 +0100

    api created
    Api for historic data has been created. The data can be exported to a postgreSQL db which I will be setting up next.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Weather-and-Energy-Grid-Optimization
 
+## Requirements
+ - external libraries via pip install --> requests pandas sqlalchemy psycopg2-binary
+
 
 ---
 ## Challenges
--- meteoapi.py
+++ meteoapi.py
@@ -1,20 +1,35 @@
-import requests
-import pandas as pd
+import requests  # for api calling
+import pandas as pd # for data manipulation into dataframes especially
+from sqlalchemy import create_engine # for exporting data to postgreSQL db
 
-def get_current_weather_data(latitude, longitude):
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true"
-    response = requests.get(url)
-    data = response.json()
-    return data
 
 def get_historic_weather_data(latitude, longitude, start_date, end_date):
+    # data gathered from the open-meteo api
     url = f"https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,wind_speed_10m_max"
     response = requests.get(url)
     data = response.json()
-    return data
+    
+    daily = data["daily"]
+    dataframe = pd.DataFrame(daily)
 
-data_historic = get_historic_weather_data(51.5085, -0.1257, "2025-06-20", "2026-06-20")
-print(data_historic)
+    dataframe = dataframe.rename(columns={"time": "date"}) #rename "time" column to "date"
+    dataframe["date"] = pd.to_datetime(dataframe["date"])
+    dataframe[latitude] = latitude
+    dataframe[longitude] = longitude
+    
+    print(dataframe.head())  # check the first rows of the dataframe
+    print(dataframe.info())  # checks the data types and non-null counts
+    print(dataframe.isnull().sum())  # check for missing values
 
-data_current = get_current_weather_data(51.5085, -0.1257)
-print(f"\n{data_current}")
+    
+    return dataframe
+
+df= get_historic_weather_data(51.5085, -0.1257, "2025-06-20", "2026-06-20")
+print(df.head())  # check the first rows of the dataframe
+
+# ----- exporting data the PostgreSQL database ------
+
+engine = create_engine('postgresql://username:password@localhost:5432/weather_data')  # replace with your actual database credentials
+df.to_sql('historic_weather_data', con=engine, if_exists='append', index=False)
+
+print("Data exported to PostgreSQL database successfully.")
