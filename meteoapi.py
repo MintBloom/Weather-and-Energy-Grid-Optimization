d3527d31e2db8f115dd306bfcd5fd63b875d0258
MintBloom <a.stegmann09@gmail.com>
2026-07-07 16:13:46 +0100

    got rid of the energy api
    Turns out that the historical energy demand data I want, is only available as a .csv file (so far as I am aware)

--- meteoapi.py
+++ meteoapi.py
@@ -5,7 +5,7 @@ from urllib import parse
 
 def get_historic_weather_data(latitude, longitude, start_date, end_date):
     # gathering data from the open-meteo api
-    url = f"https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,wind_speed_10m_max"
+    url = f"https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,wind_speed_10m_max,shortwave_radiation_sum,cloudcover_mean&timezone=GMT"
     response = requests.get(url)
     data = response.json()
     
@@ -23,22 +23,6 @@ def get_historic_weather_data(latitude, longitude, start_date, end_date):
 
     
     return dataframe
-
-def get_energy_data():
-    sql_query =  '''SELECT COUNT(*) OVER () AS _count, * FROM "f93d1835-75bc-43e5-84ad-12472b180a98" WHERE "HYDRO" >= '0' AND "SOLAR" >= '0' AND "WIND" >= '0' AND "DATETIME" >= '2025-06-10T00:00:00.000Z' AND "DATETIME" <= '2026-06-10T23:59:59.999Z' ORDER BY "_id" ASC LIMIT 100'''
-    params = {'sql': sql_query}
-
-    try:
-        response = requests.get('https://api.neso.energy/api/3/action/datastore_search_sql', params = parse.urlencode(params))
-        data = response.json()['result']
-        print(data) # Printing data
-    except requests.exceptions.RequestException as e:
-        print(e.response.text)
-    
-def get_carbon_intensity_data():
-    response = requests.get('https://api.carbonintensity.org.uk/regional/scotland')
-    data = response.json()
-    print(data)
 
 def export_to_postgresql(username, password, host, port, database_name, df):
     # ----- exporting data the PostgreSQL database ------
@@ -59,10 +43,8 @@ def export_to_postgresql(username, password, host, port, database_name, df):
 
 
 if __name__ == "__main__":
-    #df= get_historic_weather_data(51.5085, -0.1257, "2025-06-20", "2026-06-20")
-    #print(df.head())  # check the first rows of the dataframe
+    df= get_historic_weather_data(51.5085, -0.1257, "2025-06-20", "2026-06-20")
+    print(df.head())  # check the first rows of the dataframe
 
-    #export_to_postgresql("postgres", "", "localhost", "5432", "Weather and Energy Database", df)
+    export_to_postgresql("postgres", "#76h25T", "localhost", "5432", "Weather and Energy Database", df)
 
-
-    get_carbon_intensity_data()  
