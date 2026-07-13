2818323152e866b58e14fa9d41d744f4636dc32a
MintBloom <a.stegmann09@gmail.com>
2026-07-13 11:39:05 +0100

    Renamed the previous meteoapi.py file to be more fitting. Otherwise added a datetime column to the energy table, and now gather the weather data on an hourly basis.

--- DataGather&Export.py
+++ DataGather&Export.py
@@ -5,17 +5,17 @@ import os               # for reading files and directories
 
 def get_historic_weather_data(latitude, longitude, start_date, end_date):
     # gathering data from the open-meteo api
-    url = f"https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,snowfall_sum,wind_speed_10m_max,shortwave_radiation_sum,cloudcover_mean&timezone=GMT"
+    url = f"https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&start_date={start_date}&end_date={end_date}&hourly=temperature_2m,precipitation,rain,snowfall,wind_speed_10m,shortwave_radiation,cloudcover&timezone=GMT"
     response = requests.get(url)
     data = response.json()
     
-    daily = data["daily"]
-    dataframe = pd.DataFrame(daily)
+    hourly = data["hourly"]
+    dataframe = pd.DataFrame(hourly)
 
-    dataframe = dataframe.rename(columns={"time": "date"}) #rename "time" column to "date"
-    dataframe["date"] = pd.to_datetime(dataframe["date"])
-    dataframe[latitude] = latitude
-    dataframe[longitude] = longitude
+    dataframe = dataframe.rename(columns={"time": "datetime"}) #rename "time" column to "date"
+    dataframe["datetime"] = pd.to_datetime(dataframe["datetime"])
+    dataframe["latitude"] = latitude
+    dataframe["longitude"] = longitude
     
     print(dataframe.head())  # check the first rows of the dataframe
     print(dataframe.info())  # checks the data types and non-null counts
@@ -52,7 +52,9 @@ def read_demand_data_from_csvfile(file_path): # file_path is the path to the CSV
             print(entry.name)
             file = pd.read_csv(file_path + "\\" + entry.name)
             df = pd.DataFrame(file)
-            df["SETTLEMENT_DATE"] = pd.to_datetime(df["SETTLEMENT_DATE"], format='mixed', dayfirst=True)  # convert the "SETTLEMENT_DATE" column to datetime format
+            df["SETTLEMENT_DATE"] = pd.to_datetime(df["SETTLEMENT_DATE"], format='mixed', dayfirst=True)
+            df["SETTLEMENT_PERIOD"] = pd.to_numeric(df["SETTLEMENT_PERIOD"])
+            df["datetime"] = df["SETTLEMENT_DATE"] + pd.to_timedelta((df["SETTLEMENT_PERIOD"] - 1) * 30, unit="m") # combining the settlement_date and settlement_period columns into a single datetime column
             print(df.shape)  # show the columns and rows of the dataframe
             print(df.dtypes)  # check the data types of the columns
             print(df.head())  # check the first rows of the dataframe
@@ -73,7 +75,7 @@ def combine_demand_dataframes(list_of_dataframes):
     
 
 if __name__ == "__main__":
-    historic_weather_df = get_historic_weather_data(51.5085, -0.1257, "2025-06-20", "2026-06-20")
+    historic_weather_df = get_historic_weather_data(51.5085, -0.1257, "2020-01-01", "2026-06-16")
     export_to_postgresql("postgres", "", "localhost", "5432", "Weather and Energy Database", historic_weather_df, "history_weather_data")
 
     energy_demand_df = read_demand_data_from_csvfile("C:\\Code\\Github\\Weather-and-Energy-Grid-Optimization\\energy demand data")
--- README.md
+++ README.md
@@ -6,10 +6,14 @@
 
 ---
 ## Challenges
- - Learning to use Pandas DataFrames
- - Exporting DF to postgreSQL
+ - Learning to use Pandas DataFrames and exporting DF to postgreSQL
+    My first time using most if not all of these data-orientated python modules, so it took me a fair bit to write the DataGather&Export.py script
  - How to actually forecast the weather from the data I've gathered
+    
  - What "kind" of database to use for energy
+    going with the csv files for the UK wide demand. the api documentation seemed more strenuous to use, or at least finding information on how to use it was tiring so opted for the csv datasets instead.
 
- - what time increments to use for analysis (hourly, daily, etc..)
- 
+ - what time increments/frequency to use for analysis (hourly, daily, etc..)
+    going with at least hourly data, but will briefly look at weather and energy data first. Based on how detailed the trends and spikes in each set are, I will switch both to either hourly or half-hourly
+ - where to analyse and plot graphs of my data
+    jupyter notebooks?
